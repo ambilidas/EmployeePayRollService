74992da8c21b624cb42e0393489bb0116462740f
Ambili das v s <ambili.kan@gmail.com>
2022-06-19 17:03:01 +0530

    Program is modified for updating salary of an employee

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace EmployeePayRollService
             EmployeeRepo repo = new EmployeeRepo();
             Console.WriteLine("1.Get data from database employee payroll");
             Console.WriteLine("2.Add data to database employee payroll");
+            Console.WriteLine("3.Update the salary for employee and update with database");
             Console.Write("Enter your choice : ");
             int choice =Convert.ToInt32(Console.ReadLine());
             switch (choice)
@@ -22,6 +23,10 @@ namespace EmployeePayRollService
                 case 2:
                     repo.AddEmployee();
                     break ;
+                case 3:
+                    SalaryUpdateModel salaryUpdateModel = new SalaryUpdateModel();
+                    Salary.UpdateEmployeeSalary(salaryUpdateModel);
+                    break;
                 default:
                     Console.WriteLine("Invalid entry");
                     break ;
---
+++ Salary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace EmployeePayRollService
+{
+    public class Salary
+    {
+        private static SqlConnection ConnectionSetUp()
+        {
+            return new SqlConnection("data source=LIANO; database=Payroll_Service; integrated security=true");
+        }
+        public static long UpdateEmployeeSalary(SalaryUpdateModel salaryUpdateModel)
+        {
+            SqlConnection SalaryConnection = ConnectionSetUp();
+            long salary = 0;
+            try
+            {
+                using (SalaryConnection)
+                {
+                    SalaryDetailModel displayModel = new SalaryDetailModel();
+                    //SalaryUpdateModel salaryUpdateModelobj= new SalaryUpdateModel();
+                    SalaryUpdateModel UpdateModel = new SalaryUpdateModel();
+                  
+                    //Define sql command object
+                    SqlCommand cmd = new SqlCommand("spUpdateEmployeeSalary",SalaryConnection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmployeeName", UpdateModel.EmployeeName);
+                    cmd.Parameters.AddWithValue("@EmployeeName", UpdateModel.Salary);
+
+                    SalaryConnection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    //check if there are recors
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            displayModel.EmployeeName = reader["empName"].ToString();
+                            displayModel.Salary = Convert.ToInt64(reader["salary"]);
+
+                            //display retrieved records
+                            Console.WriteLine("{0},{1}",displayModel.EmployeeName,displayModel.Salary);
+                            Console.WriteLine("\n");
+                            salary = displayModel.Salary;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    //close data reader
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                SalaryConnection.Close();
+            }
+            return salary;
+        }
+    }
+}
